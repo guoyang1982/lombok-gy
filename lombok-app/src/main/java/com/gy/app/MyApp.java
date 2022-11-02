3edc5adfedb5b456d8b5c17e7d2ab38952ef5486
郭阳(guoyang04) <guoyang04@qiyi.com>
2022-11-02 18:07:27 +0800

    tijao

--- lombok-app/src/main/java/com/gy/app/MyApp.java
+++ lombok-app/src/main/java/com/gy/app/MyApp.java
@@ -4,6 +4,7 @@ import com.gy.annotation.AddHelloWorld;
 import com.gy.annotation.Getter;
 import com.gy.annotation.Setter;
 import com.gy.annotation.ShowHelloWorld;
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion.User;
 
 /**
  * @author guoyang
@@ -17,9 +18,10 @@ private String value;
 @AddHelloWorld
 public static void main(String[] args){
     MyApp app = new MyApp();
-
-
-
+    System.out.println("dddd");
+    UserDto userDto = new UserDto();
+    userDto.setName("sss");
+    System.out.println(userDto.getName());
 }
 
 }
---
+++ lombok-app/src/main/java/com/gy/app/UserDto.java
@@ -0,0 +1,14 @@
+package com.gy.app;
+
+import com.gy.annotation.Getter;
+import com.gy.annotation.Setter;
+
+/**
+ * @author guoyang
+ * @date 2022/11/2 3:54 下午
+ */
+@Getter
+@Setter
+public class UserDto {
+private String name;
+}
